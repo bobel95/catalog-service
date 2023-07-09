2e33c1c5cc4fbe7110160886e0ff82b1ab263075
Iulian Popa <iulianpopa.i@outlook.com>
2023-07-09 03:08:49 +0300

    Add integration test w/ web app context

--- src/test/java/com/bookshop/catalog/CatalogApplicationTests.java
+++ src/test/java/com/bookshop/catalog/CatalogApplicationTests.java
@@ -1,13 +1,44 @@
 package com.bookshop.catalog;
 
+import com.bookshop.catalog.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void whenPostRequest_bookIsCreated() {
+
+		var expectedBook = new Book(
+				"1234567890",
+				"Title",
+				"Author",
+				10.99
+		);
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(Book.class).value(actualBook -> {
+					assertNotNull(actualBook);
+					assertEquals( expectedBook.isbn(), actualBook.isbn());
+				});
+
+	}
 }
